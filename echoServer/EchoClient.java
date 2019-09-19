9e20466874ceab42c72dab7cee7b8aa9176a2ff6
xiaowu <liulimengplay@163.com>
2019-09-19 13:42:28 +0800

    转为utf-8编码, 并添加了ServerSocket构造函数相关的注释与代码

--- echoServer/EchoClient.java
+++ echoServer/EchoClient.java
@@ -3,37 +3,37 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 /*
-Socket�����¼���ѡ��
-TCP_NODELAY: ��ʾ������������
-SO_RESUSEADDR: ��ʾ�Ƿ���������Socket���󶨵ı��ص�ַ
-SO_TIMEOUT: ��ʾ��������ʱ�ĵȴ���ʱʱ��
-SO_LINGER: ��ʾ��ִ��Socket��close()����ʱ���Ƿ������رյײ��Socket
-SO_RCVBUF��SO_SNDBUF��: ��ʾ���루��������ݵĻ������Ĵ�С
-			���ã�public void setReceiveBufferSize(int size)
-			��ȡ��public int getReceiveBufferSize()
-			һ����˵�����������������ݿ飨����HTTP��FTPЭ���ͨ�ţ�����ʹ�ýϴ�Ļ��������������Լ��ٴ������ݵĴ�������ߴ������ݵ�����
-			�����ڽ���Ƶ���ҵ��δ����������Ƚ�С��ͨ�ŷ�ʽ��Telnet��������Ϸ������Ӧ�ò���С�Ļ�������ȷ��С�����������ܹ���ʱ�ط��͸��Է�
-SO_KEEPALIVE: ��ʾ���ڳ�ʱ�䴦�ڿ���״̬��Socket���Ƿ�Ҫ�Զ������ر�
-				Ϊtrueʱ����ʾ�ײ��TCPʵ�ֻ���Ӹ������Ƿ���Ч�������Ӵ��ڿ���״̬�����ӵ�����û�л��ഫ�����ݣ�������2��Сʱʱ��
-				���ص�TCPʵ�ֻᷢ��һ�����ݰ���Զ�̵�Socket�����Զ�̵�Socketû�з�����Ӧ��TCPʵ�־ͻ��������11���ӣ�ֱ�����յ���ӦΪֹ��
-				�����12������û���յ���Ӧ��TCPʵ�־ͻ��Զ��رձ��ص�Socket���Ͽ����ӡ�
-				��ͬ������ƽ̨�ϣ�ʱ�޻��������
-			����Ĭ��Ϊfalse������Ŀͻ��˿��ܻ����ô��ڣ�������ע�⵽�������Ѿ�������
+Socket有以下几个选项
+TCP_NODELAY: 表示立即发送数据
+SO_RESUSEADDR: 表示是否允许重用Socket所绑定的本地地址
+SO_TIMEOUT: 表示接收数据时的等待超时时间
+SO_LINGER: 表示当执行Socket的close()方法时，是否立即关闭底层的Socket
+SO_RCVBUF（SO_SNDBUF）: 表示输入（输出）数据的缓冲区的大小
+			设置：public void setReceiveBufferSize(int size)
+			读取：public int getReceiveBufferSize()
+			一般来说，传输大的连续的数据块（基于HTTP或FTP协议的通信）可以使用较大的缓冲区，这样可以减少传输数据的次数，提高传输数据的速率
+			而对于交互频繁且单次传送数据量比较小的通信方式（Telnet和网络游戏），则应该采用小的缓冲区，确保小批量的数据能够及时地发送给对方
+SO_KEEPALIVE: 表示对于长时间处于空闲状态的Socket，是否要自动把它关闭
+				为true时，表示底层的TCP实现会监视该连接是否有效。当连接处于空闲状态（连接的两端没有互相传送数据）超过了2个小时时，
+				本地的TCP实现会发送一个数据包给远程的Socket，如果远程的Socket没有发回响应，TCP实现就会持续尝试11分钟，直到接收到响应为止。
+				如果在12分钟内没有收到响应，TCP实现就会自动关闭本地的Socket，断开连接。
+				不同的网络平台上，时限会有所差别。
+			该项默认为false，不活动的客户端可能会永久存在，而不会注意到服务器已经崩溃。
 			if(!socket.getKeepAlive()) socket.setKeepAlive(true);
-OOBINLINE: ��ʾ�Ƿ�֧�ַ���һ���ֽڵ�TCP��������
-			Socket���sendUrgentData(int data)�������ڷ���һ���ֽڵ�TCP�������ݡ�
-			����Ĭ��Ϊfalse������������£������շ��յ���������ʱ�����κδ�����ֱ�ӽ��䶪����
-			Ӧ��ע����ǣ�����ʹ��һЩ���߲�ε�Э�飬������շ������������ݵ������ǳ����ޣ����շ�����������ͨ������������ݡ�
-��������ѡ�	public void setTrafficClass(int trafficClass)
+OOBINLINE: 表示是否支持发送一个字节的TCP紧急数据
+			Socket类的sendUrgentData(int data)方法用于发送一个字节的TCP紧急数据。
+			该项默认为false，在这种情况下，当接收方收到紧急数据时不做任何处理，直接将其丢弃。
+			应当注意的是，除非使用一些更高层次的协议，否则接收方处理紧急数据的能力非常有限，接收方很难区分普通数据与紧急数据。
+服务类型选项：	public void setTrafficClass(int trafficClass)
 				public int getTrafficClass()
-				�ͳɱ���0x02	�߿ɿ��ԣ�0x04	�����������0x08	��С�ӳ�0x10
-				���磺���´����������߿ɿ��Ժ���С�ӳٴ������
+				低成本：0x02	高可靠性：0x04	最高吞吐量：0x08	最小延迟0x10
+				例如：以下代码可以请求高可靠性和最小延迟传输服务：
 					socket.setTrafficClass(0x04 | 0x10);
-�趨����ʱ�䡢�ӳٺʹ����������Ҫ�ԣ�	public void setPerformancePreferences(int connectionTime, int latency, int bandwidth)
-										connectionTime	��ʾ������ʱ�佨������
-										latency			��ʾ��С�ӳ�
-										bandwidth		��ʾ��ߴ���
-										���Ը��������������������������Դ�С������Ӧ�����������Ҫ��
+设定连接时间、延迟和带宽的相对重要性：	public void setPerformancePreferences(int connectionTime, int latency, int bandwidth)
+										connectionTime	表示用最少时间建立连接
+										latency			表示最小延迟
+										bandwidth		表示最高带宽
+										可以给三个参数赋予任意的整数，相对大小决定相应参数的相对重要性
 */
 public class EchoClient {
 	
@@ -43,37 +43,37 @@ public class EchoClient {
 	
 	public EchoClient() throws IOException {
 		/*
-		 * ����host��ʾEchoServer�������ڵ����������֣�����port��ʾEchoServer���̼����Ķ˿ڡ�
-		 * ������host��ȡֵΪ��localhost��ʱ����ʾEchoClient��EchoServer����������ͬһ�������ϡ�
+		 * 参数host表示EchoServer进程所在的主机的名字，参数port表示EchoServer进程监听的端口。
+		 * 当参数host的取值为“localhost”时，表示EchoClient与EchoServer进程运行在同一个主机上。
 		 */
 		socket = new Socket(host, port);
 		
 		
 		/*
-		 * Socket�໹���������췽����������ʽ�����ÿͻ��˵�IP��ַ�Ͷ˿�
-		 *  ���һ������ͬʱ�����������ϵ����磬���Ϳ���ӵ���������ϵ�IP��ַ��Internet�к;������У�
+		 * Socket类还有两个构造方法，允许显式地设置客户端的IP地址和端口
+		 *  如果一个主机同时属于两个以上的网络，他就可能拥有两个以上的IP地址（Internet中和局域网中）
 		 */	
 //		socket = new Socket(InetAddress address, int port, InetAddress localAddr,int localPort) throws IOException;
 //		socket = new Socket(String host, int port, InetAddress localAddr, int localPort) throws IOException;
 		
 		
 		/*
-		 * ���·�ʽ�����趨�ͻ��˵ȴ��������ӵĳ�ʱʱ�䣬
-		 * ���޶�ʱ���ڣ����ӳɹ�����connect()����˳������
-		 * �޶�ʱ���ڣ�����ĳ���쳣�����׳����쳣
-		 * �����޶�ʱ�䣬��û�����ӳɹ���Ҳû�г��������쳣����ô���׳�SocketTimeoutException
-		 * ���timeout��Ϊ0����ʾ��Զ���ᳬʱ
+		 * 以下方式可以设定客户端等待建立连接的超时时间，
+		 * 若限定时间内，连接成功，则connect()方法顺利返回
+		 * 限定时间内，出现某种异常，则抛出该异常
+		 * 超过限定时间，既没有连接成功，也没有出现其他异常，那么会抛出SocketTimeoutException
+		 * 如果timeout设为0，表示永远不会超时
 		 */
 //		socket = new Socket();
 //		SocketAddress remoteAddr = new InetSocketAddress(host, port);
 //		socket.connect(remoteAddr, 600);
 		
 		/*
-		 * Socket�Ĺ��췽���������ӷ�����ʱ�����ܻ��׳�������쳣����ΪIOException��ֱ�ӻ������ࣩ��
-		 * ������UnknownHostException������޷�ʶ�����������ֻ�IP��ַ���ͻ��׳������쳣
-		 * ������ConnectException������û�з��������̼����ƶ��Ķ˿ڣ����߷��������ܾ̾����ӣ��ͻ��׳������쳣
-		 * ������SocketTimeoutException������ȴ����ӳ�ʱ���ͻ��׳������쳣
-		 * ������BindException������޷���Socket������ָ���ı���IP��ַ��˿ڰ󶨣��ͻ��׳������쳣
+		 * Socket的构造方法请求连接服务器时，可能会抛出下面的异常（均为IOException的直接或间接子类）：
+		 * ···UnknownHostException：如果无法识别主机的名字或IP地址，就会抛出这种异常
+		 * ···ConnectException：若果没有服务器进程监听制定的端口，或者服务器进程拒绝连接，就会抛出这种异常
+		 * ···SocketTimeoutException：如果等待连接超时，就会抛出这种异常
+		 * ···BindException：如果无法把Socket对象与指定的本地IP地址或端口绑定，就会抛出这种异常
 		 */
 	}
 	
--- echoServer/EchoServer.java
+++ echoServer/EchoServer.java
@@ -3,27 +3,36 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	//���port = 0, ��ʾ�ɲ���ϵͳ������һ��������õĶ˿�, Ҳ��Ϊ�����˿�
+	//如果port = 0, 表示由操作系统来分配一个任意可用的端口, 也称为匿名端口
 	private int port = 8000;
 	private ServerSocket serverSocket;
-	/*����һ���ɲ���ϵͳ�޶�Ϊ50, �������������ʹ�ò���ϵͳ�޶�����󳤶�
-		backlog > ����ϵͳ�޶��Ķ��е���󳤶�
+	/*该项一般由操作系统限定为50, 以下三种情况会使用操作系统限定的最大长度:
+		backlog > 操作系统限定的队列的最大长度
 		backlog <= 0
-		��ServerSocket�Ĺ��췽����û������backlog���� 
+		在ServerSocket的构造方法中没有设置backlog参数 
 	 */
 	private int maxConnectNum = 2;
 	
 	public EchoServer() throws IOException {
 		/*
-		 * ServerSocket�Ĺ��췽�������ڲ���ϵͳ�аѵ�ǰ����ע��Ϊ���������̡�
+		 * ServerSocket的构造方法负责在操作系统中把当前进程注册为服务器进程。
 		 */
-		serverSocket = new ServerSocket(port, maxConnectNum);	//����8000�˿ڣ��ڶ������� backlog Ϊ����������еĳ���
-		System.out.println("����������");
+		serverSocket = new ServerSocket(port, maxConnectNum);	//监听8000端口，第二个参数 backlog 为连接请求队列的长度
+		
+		//设定绑定的IP地址(当有两个网卡,一个internnet一个局域网, 如果想让服务器只运行在局域网上, 那么就使用局域网的IP地址)
+		// serverSocket = new ServerSocket(port, maxConnectNum, InetAddress.getByName("xxx.xxx.xxx.xxx"));
+		
+		//默认的构造函数  当想要在绑定前设置ServerSocket一些选项时, 使用默认的构造函数, 例如:
+		// serverSocket = new ServerSocket();
+		// serverSocket.setReuseAddress(true);
+		// serverSocket.bind(new InetSocketAddress(8000));
+		
+		System.out.println("服务器启动");
 	}
 	
 	public EchoServer(int somePort) throws IOException {
 		serverSocket = new ServerSocket(somePort, maxConnectNum);
-		System.out.println("��ȡ����������");
+		System.out.println("读取服务器启动");
 	}
 	
 	public String echo(String msg) {
@@ -45,10 +54,10 @@ public class EchoServer {
 			Socket socket = null;
 			try {
 				/*
-				 * �������������������ServerSocket�����accept�������÷���һֱ�����˿ڣ��ȴ��ͻ�����������
-				 * ������յ�һ����������accept()�����ͻ᷵��һ��Socket�������Socket������ͻ��˵�Socket�����γ���һ��ͨ����·
+				 * 服务器程序接下来调用ServerSocket对象的accept方法，该方法一直监听端口，等待客户的连接请求，
+				 * 如果接收到一个连接请求，accept()方法就会返回一个Socket对象，这个Socket对象与客户端的Socket对象形成了一条通信线路
 				 */
-				socket = serverSocket.accept();		//�ȴ��ͻ�����������
+				socket = serverSocket.accept();		//等待客户的连接请求
 				System.out.println("New connection accepted " + socket.getInetAddress() + ":" + socket.getPort());
 				BufferedReader br = getReader(socket);
 				PrintWriter pw = getWriter(socket);
