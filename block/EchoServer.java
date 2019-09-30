3c76b9544b4c4c5d11a172b8cfc853d6d3cf16b7
xiaowu <liulimengplay@163.com>
2019-09-30 12:03:59 +0800

    使用SocketChannel的read()方法构建了readLine()方法, 本质上与BufferedReader的readLine()方法相同

--- block/EchoServer.java
+++ block/EchoServer.java
@@ -65,6 +65,9 @@ class Handler implements Runnable{
 			PrintWriter pw = getWriter(socket);
 			
 			String msg = null;
+			//SocketChannel实际上也提供了read(ByteBuffer buffer), 但用来读取一行字符串比较麻烦, 此为自定义的readLine()方法
+			// while((msg = readLine(socketChannel)) != null) {
+			//此为BufferedReader的readLine()方法
 			while((msg = br.readLine()) != null) {
 				System.out.println(msg);
 				pw.println(echo(msg));
@@ -93,5 +96,44 @@ class Handler implements Runnable{
 	}
 	private String echo(String msg) {
 		return "Echo:" + msg;
+	}
+	
+	//此为根据socketChannel的read()方法构建的readLine()方法
+	public String readLine(SocketChannel socketChannel) throws IOException {
+		//存放所有读到的数据, 假定一行字符串对应的字节序列的长度小于1024
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		//存放一次读到的数据, 一次只读一个字节
+		ByteBuffer tempBuffer = ByteBuffer.allocate(1);
+		boolean isLine = false;						//表示是否读到了一行字符串
+		boolean isEnd = false;						//表示是否到达了输入流的末尾
+		String data = null;
+		while(!isLine && !isEnd) {
+			tempBuffer.clear();						//清空缓冲区
+			//在阻塞模式下, 只有等读到了1个字节或者读到输入流末尾才返回
+			//在非阻塞模式下, 有可能返回零
+			int n = socketChannel.read(tempBuffer);
+			if (n == -1) {
+				isEnd = true;
+				break;
+			}
+			if (n == 0)
+				continue;
+			tempBuffer.flip();					//把极限设为位置, 把位置设为0
+			buffer.put(tempBuffer);				//把tempBuffer中的数据复制到buffer中
+			buffer.flip();
+			Charset charset = Charset.forName("GBK");
+			CharBuffer charBuffer = charset.decode(buffer);		//解码
+			data = charBuffer.toString();
+			if (data.indexOf("\r\n") != -1) {
+				isLine = true;
+				data = data.substring(0, data.indexOf("\r\n"));
+				break;
+			}
+			buffer.position(buffer.limit());		//把位置设为极限, 为下次读数据做准备
+			buffer.limit(buffer.capacity());		//把极限设为容量, 为下次读数据做准备
+		}
+		//如果读入了一行字符串, 就返回这行字符串, 不包括行结束符 "\r\n"
+		//如果读入了输入流的末尾, 就返回null
+		return data;
 	}
 }
